40e01375ba262f327bcf4b31c989f8efc9f0ab70
Torsten Löbner <loebnert@gmail.com>
2016-01-15 19:58:21 +0100

    removed cunit-framework

--- src/CCalc.c
+++ src/CCalc.c
@@ -1,8 +1,17 @@
 
 #include <stdio.h>
+int add(int var1,int var2){return(var1 + var2);}
+int sub(int var1,int var2){return(var1 - var2);}
+int mul(int var1,int var2){return(var1 * var2);}
+int div(int var1,int var2){return(var1 / var2);}
+
 
 int main()
 {
-	printf("Hallo Welt");
+       int a = add(3,5);
+       int b = sub(3,5);
+       int c = mul(3,5);
+       int d = div(3,5);
+	printf("Results <- %i - %i - %i - %i ->",a,b,c,d);
 	return 0;
 }
